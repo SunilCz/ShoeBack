2f61f33311c9980445e2eb6ae9474904002e0982
SunilCz <sunil.chaudhary7281@gmai.com>
2023-06-01 22:42:05 +0545

    first commit

---
+++ src/api/stripeid/controllers/stripeid.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stripeid controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::stripeid.stripeid');
